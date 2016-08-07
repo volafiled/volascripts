c29c9812c00040d8f91ba8ec56990bcf75a6ac6f
RealDolos <RealDolos@users.noreply.github.com>
2016-08-07 09:29:36 +0200

    yay

--- volaban.user.js
+++ volaban.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VolaBan
 // @namespace    http://jew.dance/
-// @version      0.5
+// @version      0.6
 // @description  Filter annoying users aka MercWMouth
 // @author       RealDolos
 // @match        https://volafile.io/r/*
@@ -9,184 +9,178 @@
 // @run-at       document-start
 // ==/UserScript==
 
-"use strict";
+(function() {
+    "use strict";
 
-const basebans = {
-  staff: ["News", "MercWMouth"],
-  exact: ["DeadPool", "Wade"],
-  whites: ["real", "dolos"]
-};
+    const basebans = {
+        staff: ["News"],
+        exact: ["DeadPool", "Wade"],
+        whites: ["real", "dolos"]
+    };
 
+    console.log("running", GM_info.script.name, GM_info.script.version);
 
-console.log("running", GM_info.script.name, GM_info.script.version);
+    let bans;
 
-let bans;
+    const make = function() {
+        let base = JSON.parse(JSON.stringify(basebans));
+        bans = JSON.parse(localStorage.getItem("bans"));
+        if (!bans) {
+            bans = base;
+        }
+        else {
+            bans = Object.setPrototypeOf(bans, base);
+        }
 
-let make = function() {
-    let base = JSON.parse(JSON.stringify(basebans));
-    bans = JSON.parse(localStorage.getItem("bans"));
-    if (!bans) {
-        bans = base;
-    }
-    else {
-        bans = Object.setPrototypeOf(bans, base);
-    }
-
-    for (let key in basebans) {
-        bans["r" + key] = key == "whites" ?
-            new RegExp(`(?:${bans[key].join("|")})`, "i") :
+        for (let key in basebans) {
+            bans["r" + key] = key == "whites" ?
+                new RegExp(`(?:${bans[key].join("|")})`, "i") :
             new RegExp(`^(?:${bans[key].join("|")})$`, "i");
-    }
-    console.log(bans);
-};
-make();
-
-let save = function() {
-    localStorage.setItem("bans", JSON.stringify(bans));
-};
-
-let handle = function(what, type, val) {
-    if (!val) {
-        return;
-    }
-    let a = bans[what == "w" ? "whites" : (what == "s" ? "staff" : "exact")];
-    if (type == "block") {
-        if (a.indexOf(val) < 0) {
-            a.push(val);
         }
-    }
-    else if (type == "unblock") {
-        let index = a.indexOf(val);
-        if (index > 0) {
-            a.splice(index, 1);
-        }
-    }
-    save();
+        console.log(bans);
+    };
     make();
-};
 
-WebSocket = window.WebSocket = (function(ws) {
-    function ignore(m) {
-        if (!m.startsWith("4")) {
-            return false;
-        }
-        m = JSON.parse(m.substr(1));
-        if (!m) {
-            return false;
-        }
-        m = m[1];
-        if (!m) {
-            return false;
-        }
-        m = m[0];
-        if (!m) {
-            return false;
-        }
-        m = m[1];
-        if (!m || m[0] !== "chat") {
-            return false;
-        }
-        m = m[1];
-        return bans.rexact.test(m.nick) ||
-            (m.options.staff && bans.rstaff.test(m.nick)) ||
-            (!(m.options.staff || m.options.user) && bans.rwhites.test(m.nick));
-    }
+    const save = function() {
+        localStorage.setItem("bans", JSON.stringify(bans));
+    };
 
-    // I'd usually use a proxy, but thanks Chrome for not supporting ecma-6
-    let WebSocket = function(url, protocols) {
-        this.ws = new ws(url, protocols);
-        this.ws.onmessage = function(e) {
-            try {
-                if (ignore(e.data)) {
-                    console.log("filtered", e.data);
-                    return true;
+    const ignore = (nick, options) => {
+        return bans.rexact.test(nick) ||
+            (options.staff && bans.rstaff.test(nick)) ||
+            (!(options.staff || options.user) && bans.rwhites.test(nick));
+    };
+
+    addEventListener("DOMContentLoaded", function domload(e) {
+        removeEventListener("DOMContentLoaded", domload, true);
+
+        let exts = null;
+        addEventListener("load", function _load() {
+            removeEventListener("load", _load);
+            exts = Room.prototype._extensions.connection.prototype.room.extensions;
+        });
+
+        const appendMessage = (user, message) => {
+            exts.chat.showMessage(user, message, {
+                dontsave: true,
+                staff: true,
+                highlight: true
+            });
+        };
+
+        const chatp = Room.prototype._extensions.chat.prototype;
+
+        // Will get rid of messages but not of notifications
+        const showMessage = chatp.showMessage;
+        chatp.showMessage = function(nick, message, options, ...args) {
+            if (ignore(nick, options)) {
+                console.error("ignored", nick, JSON.stringify(message), JSON.stringify(options));
+                return false;
+            }
+            return showMessage.apply(this, [nick, message, options].concat(args));
+        };
+
+        const _ban = (what, type, who) => {
+            if (!who) {
+                return false;
+            }
+            who = who.trim();
+            let a = bans[what == "w" ? "whites" : (what == "s" ? "staff" : "exact")];
+            if (type == "block") {
+                if (a.indexOf(who) < 0) {
+                    a.push(who);
                 }
             }
-            catch (ex) {
-                console.error(e, ex);
+            else if (type == "unblock") {
+                let index = a.indexOf(who);
+                if (index > 0) {
+                    a.splice(index, 1);
+                }
             }
-            return this.onmessage && this.onmessage(e);
-        }.bind(this);
-    };
-    WebSocket.prototype = {
-        get url() { return this.ws.url; },
-
-        CONNECTING: ws.CONNECTING,
-        OPEN: ws.OPEN,
-        CLOSING: ws.CLOSING,
-        CLOSED: ws.CLOSED,
-
-        get readyState() { return this.ws.readyState; },
-        get bufferedAmount() { return this.ws.ufferedAmount; },
-
-        get onopen() { return this.ws.onopen; },
-        set onopen(nv) { return this.ws.onopen = nv; },
-        get onerror() { return this.ws.onerror; },
-        set onerror(nv) { return this.ws.onerror = nv; },
-        get onclose() { return this.ws.onclose; },
-        set onclose(nv) { return this.ws.onclose = nv; },
-        get extensions() { return this.ws.extensions; },
-        get protocol() { return this.ws.protocol; },
-
-        close: function(code, reason) { this.ws.close(code || 1000, reason || ""); },
-
-        // onmessage handled in ctor
-        get binaryType() { return this.ws.binaryType; },
-        set binaryType(nv) { return this.ws.binaryType = nv; },
-
-        send: function(data) { this.ws.send(data); }
-    };
-    return WebSocket;
-})(window.WebSocket);
-
-addEventListener("load", function load() {
-    removeEventListener("load", load, false);
-
-    let msgs = document.querySelector('#chat_messages');
-    let appendMessage = function (user, msg) {
-        let div = document.createElement('div');
-        div.setAttribute('class', 'chat_message user profile');
-        let a = document.createElement('a');
-        a.setAttribute('class', 'username');
-        a.textContent = user + ':';
-        div.appendChild(a);
-        let span = document.createElement('span');
-        span.setAttribute('class', 'chat_text');
-        span.textContent = msg;
-        div.appendChild(span);
-        msgs.appendChild(div);
-        msgs.scrollTop = msgs.scrollHeight;
-    };
-
-    let chat = document.querySelector('#chat_input');
-    let cancel = function(e) {
-        chat.value = ''; // cancel out so it won't get send, ever
-        e.stopPropagation();
-        e.preventDefault();
-    };
-    chat.addEventListener('keydown', function (e) {
-        if (e.keyCode !== 13 || e.shiftKey || e.ctrlKey || e.altKey || e.metaKey) {
-            return;
-        }
-        let m = /^[/.]([sw]?)((?:un)?block)\s+([^\s]+)$/.exec(chat.value);
-        if (m) {
-            cancel(e);
-            let what = m[1];
-            let type = m[2];
-            let val = m[3];
-            handle(what, type, val);
-            appendMessage("VolaBan", "modified rules for " + val);
-        }
-        else if (chat.value == "/blockreset") {
-            cancel(e);
-            localStorage.removeItem("bans");
+            save();
             make();
-            appendMessage("VolaBan", "bans were reset!");
-        }
-        else if (chat.value == "/blocklist") {
-            cancel(e);
-            appendMessage("VolaBan", "bans: " + JSON.stringify(bans));
-        }
+            appendMessage("VolaBan", `modified rules for ${what}:${who}`);
+            return true;
+        };
+        const commands = {
+            block(e) {
+                return _ban("e", "block", e);
+            },
+            wblock(e) {
+                return _ban("w", "block", e);
+            },
+            sblock(e) {
+                return _ban("s", "block", e);
+            },
+            unblock(e) {
+                return _ban("e", "unblock", e);
+            },
+            wunblock(e) {
+                return _ban("w", "unblock", e);
+            },
+            sunblock(e) {
+                return _ban("s", "unblock", e);
+            },
+            blockreset() {
+                localStorage.removeItem("bans");
+                make();
+                appendMessage("VolaBan", "bans were reset!");
+                return true;
+            },
+            blocklist() {
+                let m = new window.Array();
+                m.push({
+                    "type": "text",
+                    "value": `bans:`
+                });
+                m.push({"type": "break" });
+                for (let i of ["whites", "exact", "staff"]) {
+                    m.push({
+                        "type": "text",
+                        "value": `${i}: ${JSON.stringify(bans[i])}`
+                    });
+                    m.push({"type": "break" });
+                }
+                appendMessage("VolaBan", m);
+                return true;
+            },
+        };
+
+        // hook the original command processor
+        const onCommand = chatp.onCommand;
+        chatp.onCommand = function(command, e, ...args) {
+            let fn = commands[command];
+            if (fn && fn.call(commands, e, args)) {
+                return;
+            }
+            args.unshift(e);
+            args.unshift(command);
+            return onCommand.apply(this, args);
+        };
+
+    }, true /* need to get before vola*/ );
+
+    addEventListener("load", function load() {
+        removeEventListener("load", load, false);
+        let exts = Room.prototype._extensions.connection.prototype.room.extensions;
+
+        // Hook the notifications listener too, which is an
+        // anonymous function from a closure and hence we
+        // have to find it within the registered events
+        exts.connection._events.chatMessage.some(e => {
+            const onChatMessage = e.fn;
+            if (!/sage/.test("" + onChatMessage)) {
+                return false;
+            }
+            e.fn = function(...args) {
+                if (ignore(args[0], args[1], args[2])) {
+                    return false;
+                }
+                return onChatMessage.apply(this, args);
+            };
+            return true;
+        });
+
+        console.log("loaded");
     }, false);
-    console.log("loaded");
-}, false);
+})();
