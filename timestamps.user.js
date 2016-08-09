055581a8f260d12e4b6ad16487fb2249157da4ed
RealDolos <RealDolos@users.noreply.github.com>
2016-08-09 08:05:01 +0200

    toggle time stamps
    because roboCOP said so

--- timestamps.user.js
+++ timestamps.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Vola Timestamps
 // @namespace    http://not.jew.dance/
-// @version      0.3
+// @version      0.4
 // @description  Dongo said to make this
 // @author       RealDolos
 // @match        https://volafile.io/r/*
@@ -16,6 +16,10 @@
 
     addEventListener("DOMContentLoaded", function domload(e) {
         removeEventListener("DOMContentLoaded", domload, true);
+
+        const config_key = `${config.room_id}-timestamps`;
+        let enabled = localStorage.getItem(config_key);
+        enabled = enabled !== "disabled";
 
         const chatp = Room.prototype._extensions.chat.prototype;
 
@@ -33,7 +37,7 @@
                 if (m.nick && m.options && m.options.timestamp) {
                     let span = document.createElement("span");
                     span.classList.add("timestamp", "username");
-                    span.style.display = "inline-block";
+                    span.style.display = enabled ? "inline-block" : "none";
                     span.style.float = "left";
                     span.style.paddingRight = "6px";
                     let d = new Date(m.options.timestamp);
@@ -52,6 +56,29 @@
                 console.error(m, ex);
             }
             return addMessage.apply(this, [m].concat(args));
+        };
+
+        const commands = {
+            ts(e) {
+                enabled = !enabled;
+                localStorage.setItem(config_key, enabled ? "enabled" : "disabled");
+                for (let el of Array.from(document.querySelectorAll(".timestamp.username"))) {
+                    el.style.display = enabled ? "inline-block" : "none";
+                }
+                return true;
+            },
+        };
+
+        // hook the original command processor
+        const onCommand = chatp.onCommand;
+        chatp.onCommand = function(command, e, ...args) {
+            let fn = commands[command];
+            if (fn && fn.call(commands, e, args)) {
+                return;
+            }
+            args.unshift(e);
+            args.unshift(command);
+            return onCommand.apply(this, args);
         };
     }, true);
 })();
