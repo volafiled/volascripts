76b77389393bb26511bd691004c8c9ab253b3069
RealDolos <RealDolos@users.noreply.github.com>
2016-10-29 01:31:43 +0200

    welp, limits
    enforce!!!!1! properly

--- youcompleteme.user.js
+++ youcompleteme.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         you complete me!
 // @namespace    https://not.jew.dance/
-// @version      0.3
+// @version      0.4
 // @description  Everybody hate a swede today!
 // @author       Your mom
 // @match        https://volafile.io/r/*
@@ -16,15 +16,20 @@
     const always = ["Dongmaster", "MercWMouth", "murcmoth", "TheJIDF", "Kreg", "roboCOP", "lain", "Hillary", "Trump"];
     const never = new Set(["News", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken"]);
     const limit = 25;
+
+    let unique = function(a) {
+        return a.filter(function(e) {
+            return !!e && (!this.has(e) && !!this.add(e));
+        }, new Set());
+    };
+
     const exts = Room.prototype._extensions.connection.prototype.room.extensions;
     exts.chatInput.addNickname = function(t) {
         let e = this.nicknames;
         if (never.has(t.toString()) || e.indexOf(t) === 0) {
             return;
         }
-        this.nicknames = [t, ...e, ...always].filter(function(e) {
-            return !!e && !never.has(e) && (!this.has(e) && !!this.add(e));
-        }, new Set()).slice(-limit);
+        this.nicknames = unique([...unique([t, ...e].filter(e => !never.has(e))).slice(-limit), ...always]);
     };
 
     // initialization order is for white Argentinians-hack
