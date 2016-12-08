5c1a338cee316aa8c5bf0ecc78eff3bb56357486
RealDolos <RealDolos@users.noreply.github.com>
2016-12-08 13:33:27 +0100

    dry it!

--- timestamps.user.js
+++ timestamps.user.js
@@ -1,84 +1,62 @@
 // ==UserScript==
 // @name         Vola Timestamps
 // @namespace    http://not.jew.dance/
-// @version      0.4
+// @version      0.6
 // @description  Dongo said to make this
 // @author       RealDolos
 // @match        https://volafile.io/r/*
 // @grant        none
+// @require      https://rawgit.com/RealDolos/volascripts/064d22df5566bda12d222822584b87dcc6a43d45/dry.js
 // @run-at       document-start
 // ==/UserScript==
 
-(function() {
-    'use strict';
+dry.once("dom", () => {
+    "use strict";
+    console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
 
-    console.log("running", GM_info.script.name, GM_info.script.version);
+    const config_key = `${dry.config.room_id}-timestamps`;
+    let enabled = localStorage.getItem(config_key);
+    enabled = enabled !== "disabled";
 
-    addEventListener("DOMContentLoaded", function domload(e) {
-        removeEventListener("DOMContentLoaded", domload, true);
-
-        const config_key = `${config.room_id}-timestamps`;
-        let enabled = localStorage.getItem(config_key);
-        enabled = enabled !== "disabled";
-
-        const chatp = Room.prototype._extensions.chat.prototype;
-
-        const showMessage = chatp.showMessage;
-        chatp.showMessage = function(nick, message, options, ...args) {
-            if (!options.timestamp) {
-                options.timestamp = Date.now();
+    dry.replaceEarly("chat", "showMessage", function(orig, nick, message, options, ...args) {
+        if (!options.timestamp) {
+            options.timestamp = Date.now();
+        }
+        return orig(...[nick, message, options].concat(args));
+    });
+    dry.replaceEarly("chat", "addMessage", function(orig, m, ...args) {
+        try {
+            if (m.nick && m.options && m.options.timestamp) {
+                let span = document.createElement("span");
+                span.classList.add("timestamp", "username");
+                span.style.display = enabled ? "inline-block" : "none";
+                span.style.float = "left";
+                span.style.paddingRight = "6px";
+                let d = new Date(m.options.timestamp);
+                span.textContent = d.toLocaleString("en-US", {
+                    hour12: false,
+                    hour: "2-digit",
+                    minute: "2-digit",
+                    second: "2-digit",
+                }) + "|";
+                span.setAttribute("title", d.toLocaleString("eu"));
+                m.timestamp_elem = span;
+                m.elem.insertBefore(span, m.elem.firstChild);
             }
-            return showMessage.apply(this, [nick, message, options].concat(args));
-        };
-
-        const addMessage = chatp.addMessage;
-        chatp.addMessage = function(m, ...args) {
-            try {
-                if (m.nick && m.options && m.options.timestamp) {
-                    let span = document.createElement("span");
-                    span.classList.add("timestamp", "username");
-                    span.style.display = enabled ? "inline-block" : "none";
-                    span.style.float = "left";
-                    span.style.paddingRight = "6px";
-                    let d = new Date(m.options.timestamp);
-                    span.textContent = d.toLocaleString("en-US", {
-                        hour12: false,
-                        hour: "2-digit",
-                        minute: "2-digit",
-                        second: "2-digit",
-                    }) + "|";
-                    span.setAttribute("title", d.toLocaleString("eu"));
-                    m.timestamp_elem = span;
-                    m.elem.insertBefore(span, m.elem.firstChild);
-                }
+        }
+        catch (ex) {
+            console.error(m, ex);
+        }
+        return orig(...[m].concat(args));
+    });
+    new class extends dry.Commands {
+        ts(e) {
+            enabled = !enabled;
+            localStorage.setItem(config_key, enabled ? "enabled" : "disabled");
+            for (let el of Array.from(document.querySelectorAll(".timestamp.username"))) {
+                el.style.display = enabled ? "inline-block" : "none";
             }
-            catch (ex) {
-                console.error(m, ex);
-            }
-            return addMessage.apply(this, [m].concat(args));
-        };
-
-        const commands = {
-            ts(e) {
-                enabled = !enabled;
-                localStorage.setItem(config_key, enabled ? "enabled" : "disabled");
-                for (let el of Array.from(document.querySelectorAll(".timestamp.username"))) {
-                    el.style.display = enabled ? "inline-block" : "none";
-                }
-                return true;
-            },
-        };
-
-        // hook the original command processor
-        const onCommand = chatp.onCommand;
-        chatp.onCommand = function(command, e, ...args) {
-            let fn = commands[command];
-            if (fn && fn.call(commands, e, args)) {
-                return;
-            }
-            args.unshift(e);
-            args.unshift(command);
-            return onCommand.apply(this, args);
-        };
-    }, true);
-})();
+            return true;
+        }
+    }();
+});
