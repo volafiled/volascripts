aad7cf9d7f68d720462fdf50b274784e5194864d
RealDolos <RealDolos@users.noreply.github.com>
2017-01-17 11:14:56 +0100

    BAN HAMMER!!!!

--- admin.iptools.user.js
+++ admin.iptools.user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name        Volafile ip address hider
+// @name        Vola IP Tools
 // @namespace   volafile.ip.hider
 // @description Hides ip addresses for mods.
 // @include     https://volafile.io/r/*
-// @version     6
+// @version     8
 // @grant       none
 // @require     https://rawgit.com/RealDolos/volascripts/4e2e4cdabf706777926b3eb04a64725c02a9b124/dry.js
 // @run-at      document-start
@@ -33,6 +33,9 @@ display: none;
             document.body.removeAttribute("noipspls");
         }
         localStorage.setItem("noipspls", state.toString());
+        if (dry.exts) {
+            dry.exts.chat.scrollState.bottom();
+        }
     };
     const toggle = () => {
         state = !state;
@@ -47,7 +50,7 @@ display: none;
     uc.parentElement.insertBefore(btn, uc.nextSibling);
     btn.addEventListener("click", toggle);
 
-    dry.replaceEarly("chat", "showMessage", function(orig, nick, message, options, ...args) {
+    dry.replaceEarly("chat", "showMessage", function(orig, nick, message, options, data, ...args) {
         try {
             if (nick === "Report" && options.staff) {
                 if (typeof(message) == "string") {
@@ -59,15 +62,16 @@ display: none;
                 let newmsg = new dry.unsafeWindow.Array();
                 message.forEach(m => {
                     if (m && m.type === "text") {
-                        const pieces = m.value.match(/^(.+)( \([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\))(.+)$/);
+                        const pieces = m.value.match(/^(.+)( \(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\))(.+)$/);
                         if (!pieces) {
                             newmsg.push(m);
                             return;
                         }
-                        newmsg.push({type: "text", value: pieces[1]});
-                        // this is secure enough since the match should make sure there is no unescaped html in there
-                        newmsg.push({type: "raw", value: `<span class="tag_key_ip">${pieces[2]}</span>`});
-                        newmsg.push({type: "text", value: pieces[3]});
+                        newmsg.push({type: "text", value: pieces[1] + pieces[4]});
+                        if (!data) {
+                            data = new dry.unsafeWindow.Object();
+                            data.ip = pieces[3];
+                        }
                         return;
                     }
                     newmsg.push(m);
@@ -78,7 +82,27 @@ display: none;
         catch (ex) {
             console.error(ex);
         }
-        return orig(...[nick, message, options].concat(args));
+        const msg = orig(...[nick, message, options, data].concat(args));
+        try {
+            if (msg.ip_elem) {
+                msg.ban_elem = document.createElement("span");
+                const hammer = document.createElement("i");
+                hammer.setAttribute("class", "chat_message_icon icon-hammer");
+                hammer.style.padding = "0";
+                msg.ban_elem.appendChild(hammer);
+                msg.ban_elem.setAttribute("class", "username clickable ban");
+                msg.ban_elem.addEventListener("click", msg.showBanWindow.bind(msg));
+                const parent = msg.nick_elem.parentElement;
+                parent.insertBefore(msg.ban_elem, parent.firstChild);
+                msg.ban_elem.setAttribute("style", msg.nick_elem.getAttribute("style"));
+                msg.ban_elem.style.padding = "0";
+                msg.ban_elem.style.paddingRight = "1ex";
+            }
+        }
+        catch (ex) {
+            console.error(ex);
+        }
+        return msg;
     });
 
     new class extends dry.Commands {
