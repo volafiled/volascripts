42afce844c9fc18748c0cf72b5866baaa6533a8f
RealDolos <RealDolos@users.noreply.github.com>
2017-02-01 00:41:25 +0100

    Actually hide reports

--- admin,unnotify-reports.user.js
+++ admin,unnotify-reports.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Unnotify reports
 // @namespace    http://not.jew.dance/
-// @version      0.1
+// @version      0.2
 // @description  because it's annoying
 // @author       RealDolos
 // @match        https://volafile.io/r/*
@@ -13,8 +13,19 @@
 dry.once("dom", () => {
     "use strict";
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
+    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) @/;
     dry.replaceEarly("chat", "showMessage", function(orig, nick, message, options, ...args) {
-        if (nick === "Report" && options.highlight && options.staff) {
+        if (nick === "Report" && options.staff) {
+            options.notify = options.highlight = false;
+            if (message[1] && message[1].value) {
+                let text = message[1].value;
+                if (text.includes("BLACKLIST") && BLACK.test(text)) {
+                    console.error(message);
+                    return false;
+                }
+            }
+        }
+        else if (nick === "Log" && options.staff) {
             options.notify = options.highlight = false;
         }
         return orig(...[nick, message, options].concat(args));
