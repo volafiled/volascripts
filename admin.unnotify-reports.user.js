d415f34bec20c302933a3e4eccfe5054aba57632
RealDolos <RealDolos@users.noreply.github.com>
2017-02-02 01:46:17 +0100

    Use new dry

--- admin.unnotify-reports.user.js
+++ admin.unnotify-reports.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Unnotify reports
 // @namespace    http://not.jew.dance/
-// @version      0.2
+// @version      0.4
 // @description  because it's annoying
 // @author       RealDolos
 // @match        https://volafile.io/r/*
 // @grant        none
-// @require      https://rawgit.com/RealDolos/volascripts/064d22df5566bda12d222822584b87dcc6a43d45/dry.js
+// @require      https://rawgit.com/RealDolos/volascripts/db222e0a836c6da9d5593c7fc93941c0e7a9d2a1/dry.js
 // @run-at       document-start
 // ==/UserScript==
 
@@ -14,20 +14,26 @@ dry.once("dom", () => {
     "use strict";
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
     const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) @/;
-    dry.replaceEarly("chat", "showMessage", function(orig, nick, message, options, ...args) {
-        if (nick === "Report" && options.staff) {
-            options.notify = options.highlight = false;
-            if (message[1] && message[1].value) {
-                let text = message[1].value;
-                if (text.includes("BLACKLIST") && BLACK.test(text)) {
-                    console.error(message);
+
+    new class extends dry.MessageFilter {
+        showMessage(orig, nick, message, options) {
+            if (nick === "Report" && options.staff) {
+                options.notify = options.highlight = false;
+                if (message[0] && message[0].value && message[0].value.includes("Enabled notifications in this room")) {
                     return false;
                 }
+                if (message[1] && message[1].value) {
+                    let text = message[1].value;
+                    if (text.includes("BLACKLIST") && BLACK.test(text)) {
+                        console.error(message);
+                        return false;
+                    }
+                }
+                return;
+            }
+            if (nick === "Log" && options.staff) {
+                options.notify = options.highlight = false;
             }
         }
-        else if (nick === "Log" && options.staff) {
-            options.notify = options.highlight = false;
-        }
-        return orig(...[nick, message, options].concat(args));
-    });
+    }();
 });
