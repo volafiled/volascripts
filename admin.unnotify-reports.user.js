eba5d3543be075d18e52c2c6bcb9e985014a6a28
RealDolos <RealDolos@users.noreply.github.com>
2017-04-26 23:24:14 +0200

    .org

--- admin.unnotify-reports.user.js
+++ admin.unnotify-reports.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Unnotify reports
 // @namespace    http://not.jew.dance/
-// @version      0.4
+// @version      0.6
 // @description  because it's annoying
 // @author       RealDolos
 // @match        https://volafile.io/r/*
+// @match        https://volafile.org/r/*
 // @grant        none
 // @require      https://rawgit.com/RealDolos/volascripts/db222e0a836c6da9d5593c7fc93941c0e7a9d2a1/dry.js
 // @run-at       document-start
@@ -13,7 +14,7 @@
 dry.once("dom", () => {
     "use strict";
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
-    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) @/;
+    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) @|periscopefollower/;
 
     new class extends dry.MessageFilter {
         showMessage(orig, nick, message, options) {
