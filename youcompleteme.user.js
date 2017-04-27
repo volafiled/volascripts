017587e8f5060e067d783f9c982f2db90422ef1f
RealDolos <RealDolos@users.noreply.github.com>
2017-04-27 02:40:16 +0200

    .org

--- youcompleteme.user.js
+++ youcompleteme.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         you complete me!
 // @namespace    https://not.jew.dance/
-// @version      0.6
+// @version      0.7
 // @description  Everybody hate a swede today!
 // @author       Your mom
 // @match        https://volafile.io/r/*
+// @match        https://volafile.org/r/*
 // @grant        none
 // @require      https://rawgit.com/RealDolos/volascripts/064d22df5566bda12d222822584b87dcc6a43d45/dry.js
 // @run-at       document-idle
@@ -15,7 +16,7 @@
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
 
     const always = ["Dongmaster", "MercWMouth", "murcmoth", "TheJIDF", "Kreg", "roboCOP", "lain"];
-    const never = new Set(["News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log"]);
+    const never = new Set(["Wombot", "News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log", "Twitter",]);
     const limit = 25;
 
     const addn = dry.replaceLate("chatInput", "addNickname", function(orig, str, ...args) {
