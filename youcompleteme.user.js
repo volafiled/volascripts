f2b955a93e91d3767cb663efe6d62362e1b48087
RealDolos <RealDolos@users.noreply.github.com>
2017-05-08 22:16:35 +0200

    recent vola changes

--- youcompleteme.user.js
+++ youcompleteme.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         you complete me!
 // @namespace    https://not.jew.dance/
-// @version      0.7
+// @version      0.8
 // @description  Everybody hate a swede today!
 // @author       Your mom
 // @match        https://volafile.io/r/*
 // @match        https://volafile.org/r/*
 // @grant        none
-// @require      https://rawgit.com/RealDolos/volascripts/064d22df5566bda12d222822584b87dcc6a43d45/dry.js
+// @require      https://rawgit.com/RealDolos/volascripts/a9c0424e5498deea9fd437c15b2137c3bec07c61/dry.js
 // @run-at       document-idle
 // ==/UserScript==
 
@@ -19,12 +19,12 @@
     const never = new Set(["Wombot", "News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log", "Twitter",]);
     const limit = 25;
 
-    const addn = dry.replaceLate("chatInput", "addNickname", function(orig, str, ...args) {
-        let e = this.nicknames;
+    const addn = dry.replaceLate("chat.chatInput", "addSuggestion", function(orig, str, ...args) {
+        let e = this.suggestions;
         if (never.has(str.toString()) || e.indexOf(str) === 0) {
             return;
         }
-        this.nicknames = dry.unique([...dry.unique([str, ...e].filter(e => !never.has(e))).slice(0, limit), ...always]);
+        this.suggestions = dry.unique([...dry.unique([str, ...e].filter(e => !never.has(e))).slice(0, limit), ...always]);
     });
     addn("");
     setTimeout(() => addn(""), 1000);
