cd848f967c5410e50e74b22fa9b38d8ade87a1e2
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-05-30 19:04:07 +0200

    cdn

--- LAINpls.user.js
+++ LAINpls.user.js
@@ -6,7 +6,7 @@
 // @author       topkuk productions
 // @match        https://volafile.org/r/*
 // @grant        unsafeWindow
-// @require      https://rawgit.com/RealDolos/volascripts/1dd689f72763c0e59f567fdf93865837e35964d6/dry.js
+// @require      https://cdn.rawgit.com/RealDolos/volascripts/1dd689f72763c0e59f567fdf93865837e35964d6/dry.js
 // @run-at       document-start
 // @icon         https://volafile.org/favicon.ico
 // ==/UserScript==
