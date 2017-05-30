95fb3979dea566a207e8aa5a202bb45bbf44f814
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-05-30 19:27:23 +0200

    cdn

--- admin.unnotify-reports.user.js
+++ admin.unnotify-reports.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Unnotify reports
-// @namespace    http://not.jew.dance/
-// @version      0.6
+// @version      2
 // @description  because it's annoying
-// @author       RealDolos
-// @match        https://volafile.io/r/*
+// @namespace    https://volafile.org
+// @icon         https://volafile.org/favicon.ico
+// @author       topkuk productions
 // @match        https://volafile.org/r/*
+// @require      https://cdn.rawgit.com/RealDolos/volascripts/1dd689f72763c0e59f567fdf93865837e35964d6/dry.js
 // @grant        none
-// @require      https://rawgit.com/RealDolos/volascripts/db222e0a836c6da9d5593c7fc93941c0e7a9d2a1/dry.js
 // @run-at       document-start
 // ==/UserScript==
 
@@ -20,9 +20,6 @@ dry.once("dom", () => {
         showMessage(orig, nick, message, options) {
             if (nick === "Report" && options.staff) {
                 options.notify = options.highlight = false;
-                if (message[0] && message[0].value && message[0].value.includes("Enabled notifications in this room")) {
-                    return false;
-                }
                 if (message[1] && message[1].value) {
                     let text = message[1].value;
                     if (text.includes("BLACKLIST") && BLACK.test(text)) {
