d782acc5600550b4ddc519fc88a1e4345a9df8bc
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-07-04 08:56:21 +0200

    MOAR suggestions

--- youcompleteme.user.js
+++ youcompleteme.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://volafile.org/
 // @author       topkuk productions
 // @icon         https://volafile.org/favicon.ico
-// @version      3
+// @version      4
 // @description  So you can better ignore people
 // @match        https://volafile.org/r/*
 // @grant        none
@@ -15,16 +15,19 @@
     'use strict';
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
 
-    const always = ["Dongmaster", "MercWMouth", "murcmoth", "TheJIDF", "Kreg", "roboCOP", "lain"];
-    const never = new Set(["Wombot", "News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log", "Twitter",]);
-    const limit = 25;
+    const always = ["Dongmaster", "MercWMouth", "TheJIDF", "Kreg", "roboCOP", "lain", "Heisenb3rg", "auxo", "MoDChatbot", "bain", "lmmortal", "someguy1992", "Red"];
+    const never = new Set(["Wombot", "News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log", "Twitter", "Record",]);
+    const limit = 30;
 
     const addn = dry.replaceLate("chat.chatInput", "addSuggestion", function(orig, str, ...args) {
         let e = this.suggestions;
         if (never.has(str.toString()) || e.indexOf(str) === 0) {
             return;
         }
-        this.suggestions = dry.unique([...dry.unique([str, ...e].filter(e => !never.has(e))).slice(0, limit), ...always]);
+        // Delay stuff a bit to avoid people stealing completes; suggestion by the Redard
+        setTimeout(() => {
+            this.suggestions = dry.unique([...dry.unique([str, ...e].filter(e => !never.has(e))).slice(0, limit), ...always]);
+        }, 1000);
     });
     addn("");
     setTimeout(() => addn(""), 1000);
