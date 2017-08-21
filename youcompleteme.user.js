115320d03700d8e43cab6b0adabf519c0ffd0693
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-08-21 21:50:34 +0200

    de-dupe, block self, etc.

--- youcompleteme.user.js
+++ youcompleteme.user.js
@@ -3,7 +3,7 @@
 // @namespace    https://volafile.org/
 // @author       topkuk productions
 // @icon         https://volafile.org/favicon.ico
-// @version      4
+// @version      5
 // @description  So you can better ignore people
 // @match        https://volafile.org/r/*
 // @grant        none
@@ -15,18 +15,41 @@
     'use strict';
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
 
-    const always = ["Dongmaster", "MercWMouth", "TheJIDF", "Kreg", "roboCOP", "lain", "Heisenb3rg", "auxo", "MoDChatbot", "bain", "lmmortal", "someguy1992", "Red"];
-    const never = new Set(["Wombot", "News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log", "Twitter", "Record",]);
+    const always = [
+        "RealDolos", "Dongmaster", "MercWMouth", "TheJIDF", "Kreg", "roboCOP", "Lain", "Heisenb3rg", "Red", "dad",
+        "auxo", "MoDChatbot", "bain", "lmmortal", "someguy1992", "evilant", "Szeraton", "NEPTvola", "EffEff", ];
+    let never = new Set(["Wombot", "News", "Network", "MOTD", "System", "CucklordSupreme", "Report", "VolaPG", "KakKakken", "Rogueian", "Log", "Twitter", "Record",]);
     const limit = 30;
-
+    
+    never = new Set(Array.from(never).map(e => e.toUpperCase()));
+    
+    const filter = function(current, e) {
+        const u = e.toUpperCase();
+        if (this.has(u) || never.has(u) || current === u) {
+            return false;
+        }
+        this.add(u);
+        return true;
+    };
+    
     const addn = dry.replaceLate("chat.chatInput", "addSuggestion", function(orig, str, ...args) {
+        const current = dry.exts.user.name.toUpperCase();
         let e = this.suggestions;
-        if (never.has(str.toString()) || e.indexOf(str) === 0) {
+        str = str.toString();
+        const ustr = str.toUpperCase();
+        if (!str || never.has(ustr) || e.indexOf(str) === 0 || ustr === current) {
             return;
         }
         // Delay stuff a bit to avoid people stealing completes; suggestion by the Redard
         setTimeout(() => {
-            this.suggestions = dry.unique([...dry.unique([str, ...e].filter(e => !never.has(e))).slice(0, limit), ...always]);
+            let newSuggestions = [...[str, ...e].slice(0, limit), ...always];
+            try {
+                newSuggestions = newSuggestions.filter(filter.bind(new Set(), current));
+                this.suggestions = newSuggestions;
+            }
+            catch (ex) {
+                console.error(ex);
+            }
         }, 1000);
     });
     addn("");
