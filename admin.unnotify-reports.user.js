51b76c05be26adca7b4a4897115f67f10d9df668
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-09-18 12:22:23 +0200

    New reports stuff

--- admin.unnotify-reports.user.js
+++ admin.unnotify-reports.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Unnotify reports
-// @version      2
+// @version      3
 // @description  because it's annoying
 // @namespace    https://volafile.org
 // @icon         https://volafile.org/favicon.ico
@@ -14,20 +14,23 @@
 dry.once("dom", () => {
     "use strict";
     console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
-    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) @|periscopefollower/;
+    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip) |periscopefollower/;
 
     new class extends dry.MessageFilter {
         showMessage(orig, nick, message, options) {
-            if (nick === "Report" && options.staff) {
+            if (typeof message !== "string" && ((nick === "Report" && options.staff) || (message && message[0] && message[0].href === "/reports"))) {
                 options.notify = options.highlight = false;
-                if (message[1] && message[1].value) {
-                    let text = message[1].value;
+                try {
+                    let text = message.map(e => e.value || "").join("");
                     if (text.includes("BLACKLIST") && BLACK.test(text)) {
-                        console.error(message);
+                        console.error(text);
                         return false;
                     }
+                    return;
                 }
-                return;
+                catch (ex) {
+                    console.error("failed", message, ex);
+                }
             }
             if (nick === "Log" && options.staff) {
                 options.notify = options.highlight = false;
