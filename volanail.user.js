82445a013b155c0088dc13aef43a975d39b0c714
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-09-26 22:25:52 +0200

    Adjust column count according to bin width

--- volanail.user.js
+++ volanail.user.js
@@ -9,7 +9,7 @@
 // @require     https://cdn.rawgit.com/RealDolos/node-4get/1be8052af5770998d6d936fdd5eb717571b205c8/lib/finally.js
 // @require     https://cdn.rawgit.com/RealDolos/node-4get/1be8052af5770998d6d936fdd5eb717571b205c8/lib/pool.js
 // @grant       none
-// @version     0.28
+// @version     1.0
 // ==/UserScript==
 /* globals GM_info, dry, format, PromisePool */
 /* jslint strict:global,browser:true,devel:true */
@@ -19,130 +19,6 @@ console.log("running", GM_info.script.name, GM_info.script.version);
 
 const error = "data:image/svg+xml;charset=utf-8,%3Csvg%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20width%3D%22200%22%20version%3D%221.1%22%3E%0D%0A%3Cdefs%3E%0D%0A%3ClinearGradient%20id%3D%22c%22%20y2%3D%225.541%22%20gradientUnits%3D%22userSpaceOnUse%22%20y1%3D%2266.29%22%20x2%3D%2219.07%22%20x1%3D%2236.92%22%3E%0D%0A%3Cstop%20stop-color%3D%22%23a40000%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23ff1717%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FlinearGradient%3E%0D%0A%3ClinearGradient%20id%3D%22b%22%20y2%3D%22-8.563%22%20gradientUnits%3D%22userSpaceOnUse%22%20y1%3D%2253.84%22%20x2%3D%2220.07%22%20x1%3D%2243.94%22%3E%0D%0A%3Cstop%20stop-color%3D%22%23ffe69b%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23fff%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FlinearGradient%3E%0D%0A%3ClinearGradient%20id%3D%22a%22%20y2%3D%2215.82%22%20gradientUnits%3D%22userSpaceOnUse%22%20y1%3D%2233.96%22%20gradientTransform%3D%22matrix%284.559%200%200%204.559%20-9.417%20-156.5%29%22%20x2%3D%2220.92%22%20x1%3D%2221.99%22%3E%0D%0A%3Cstop%20stop-color%3D%22%23fffeff%22%20stop-opacity%3D%22.3333%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23fffeff%22%20stop-opacity%3D%22.2157%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FlinearGradient%3E%0D%0A%3C%2Fdefs%3E%0D%0A%3Cg%20transform%3D%22translate%28-4.122e-8%20152%29%22%3E%0D%0A%3Cg%20transform%3D%22matrix%284.545%200%200%204.545%20-9.091%20-156.5%29%22%3E%0D%0A%3Ccircle%20stroke-width%3D%221.086%22%20transform%3D%22matrix%28.9205%200%200%20.9205%202.368%20.9741%29%22%20stroke%3D%22%23b20000%22%20cy%3D%2223.93%22%20cx%3D%2223.5%22%20r%3D%2223.36%22%20fill%3D%22url%28%23c%29%22%2F%3E%0D%0A%3Ccircle%20opacity%3D%22.3466%22%20stroke-width%3D%221.168%22%20fill-opacity%3D%220%22%20transform%3D%22matrix%28.8561%200%200%20.8561%201.818%20.1978%29%22%20stroke%3D%22url%28%23b%29%22%20cy%3D%2226.64%22%20cx%3D%2225.91%22%20r%3D%2223.99%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3C%2Fg%3E%0D%0A%3Cg%20transform%3D%22translate%28-4.122e-8%20152%29%22%3E%0D%0A%3Crect%20height%3D%2236.36%22%20width%3D%22127.3%22%20y%3D%22-70.18%22%20x%3D%2236.37%22%20fill%3D%22%23efefef%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3Cg%20transform%3D%22translate%28-4.122e-8%20152%29%22%3E%0D%0A%3Cpath%20d%3D%22m188.3-57.46c0%2049.38-47.21-28.57-85.14%201.764-37.05%2029.62-93.9%2055.58-93.9%206.2%200-50.53%2039.64-97.41%2089.02-97.41%2049.38-0.00091%2090.02%2040.07%2090.02%2089.45z%22%20fill%3D%22url%28%23a%29%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3C%2Fsvg%3E";
 const loading = "data:image/svg+xml;charset=utf-8,%3Csvg%20height%3D%22200%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20200%20200%22%20width%3D%22200%22%20version%3D%221.1%22%3E%0D%0A%3Cdefs%3E%0D%0A%3ClinearGradient%20id%3D%22e%22%20y2%3D%2252.09%22%20gradientUnits%3D%22userSpaceOnUse%22%20x2%3D%229.886%22%20gradientTransform%3D%22matrix%283.124%200%200%20.9697%20-31.89%20-19.6%29%22%20y1%3D%2237.2%22%20x1%3D%228.916%22%3E%0D%0A%3Cstop%20stop-color%3D%22%239aa29a%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23b5beb5%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FlinearGradient%3E%0D%0A%3CradialGradient%20id%3D%22b%22%20gradientUnits%3D%22userSpaceOnUse%22%20cy%3D%2210.04%22%20cx%3D%2211.9%22%20r%3D%2229.29%22%3E%0D%0A%3Cstop%20stop-color%3D%22%23fffffd%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23cbcbc9%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FradialGradient%3E%0D%0A%3ClinearGradient%20id%3D%22d%22%20y2%3D%2225.88%22%20gradientUnits%3D%22userSpaceOnUse%22%20x2%3D%2222.22%22%20y1%3D%227.789%22%20x1%3D%226.342%22%3E%0D%0A%3Cstop%20stop-color%3D%22%23a0a0a0%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23fff%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FlinearGradient%3E%0D%0A%3CradialGradient%20id%3D%22a%22%20gradientUnits%3D%22userSpaceOnUse%22%20cy%3D%2210.97%22%20cx%3D%2223.38%22%20gradientTransform%3D%22matrix%283.63%200%200%203.742%20-61.49%20-29.19%29%22%20r%3D%223.348%22%3E%0D%0A%3Cstop%20stop-color%3D%22%2363dcff%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%2300c5fe%22%20stop-opacity%3D%22.1809%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FradialGradient%3E%0D%0A%3ClinearGradient%20id%3D%22c%22%20y2%3D%225.285%22%20gradientUnits%3D%22userSpaceOnUse%22%20x2%3D%2220.33%22%20y1%3D%224.257%22%20x1%3D%2219.67%22%3E%0D%0A%3Cstop%20stop-color%3D%22%230097c3%22%20offset%3D%220%22%2F%3E%0D%0A%3Cstop%20stop-color%3D%22%23006b9c%22%20stop-opacity%3D%220%22%20offset%3D%221%22%2F%3E%0D%0A%3C%2FlinearGradient%3E%0D%0A%3C%2Fdefs%3E%0D%0A%3Cg%20transform%3D%22translate%28-9%20161%29%22%3E%0D%0A%3Cg%20transform%3D%22matrix%284.578%200%200%204.578%20-.09448%20-164.2%29%22%3E%0D%0A%3Cg%20fill-rule%3D%22evenodd%22%3E%0D%0A%3Cpath%20style%3D%22color%3A%23000000%22%20d%3D%22m18.59%201.404-14.36%2016.69%201.258%201.243%2013.1-17.94z%22%20fill%3D%22url%28%23e%29%22%2F%3E%0D%0A%3Cpath%20d%3D%22m18.47%201.314-12.81%2017.76%201.829%201.615%2010.98-19.37z%22%20fill%3D%22%23fefefe%22%2F%3E%0D%0A%3Ccircle%20stroke-linejoin%3D%22round%22%20stroke-width%3D%22.6985%22%20stroke-linecap%3D%22round%22%20transform%3D%22matrix%281.432%200%200%201.432%20.5695%20-1.655%29%22%20stroke%3D%22%23656565%22%20cy%3D%2216.91%22%20cx%3D%2216.25%22%20r%3D%2214.91%22%20fill%3D%22%236d6d6d%22%2F%3E%0D%0A%3Ccircle%20stroke-linejoin%3D%22round%22%20stroke-width%3D%22.7114%22%20stroke-linecap%3D%22round%22%20transform%3D%22matrix%281.164%200%200%201.164%204.825%202.778%29%22%20stroke%3D%22url%28%23d%29%22%20cy%3D%2216.91%22%20cx%3D%2216.25%22%20r%3D%2214.91%22%20fill%3D%22url%28%23b%29%22%2F%3E%0D%0A%3Cpath%20style%3D%22color%3A%23000000%22%20d%3D%22m16.68%206.639a8.5%208.5%200%200%201%206.65%20-3.264l0.05%208.505z%22%20transform%3D%22matrix%281.77%200%200%201.77%20-17.02%201.611%29%22%20stroke%3D%22url%28%23c%29%22%20stroke-width%3D%22.5650%22%20fill%3D%22url%28%23a%29%22%2F%3E%0D%0A%3Ccircle%20stroke-linejoin%3D%22round%22%20stroke-width%3D%22.4823%22%20stroke-linecap%3D%22round%22%20transform%3D%22matrix%282.073%200%200%202.073%20-7.31%20-13.14%29%22%20stroke%3D%22%23000%22%20cy%3D%2217.28%22%20cx%3D%2215.19%22%20r%3D%221.219%22%20fill%3D%22%23f3f3f3%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3Cpath%20d%3D%22m22.18%2020.72-9.02-7.58%22%20stroke%3D%22%23000%22%20stroke-linecap%3D%22round%22%20fill%3D%22none%22%2F%3E%0D%0A%3Cpath%20d%3D%22m19.41%2029.78%202.96-4.494%22%20stroke%3D%22%23000%22%20stroke-linecap%3D%22round%22%20stroke-width%3D%222%22%20fill%3D%22none%22%2F%3E%0D%0A%3Cg%20fill-rule%3D%22evenodd%22%20fill%3D%22%23b6b9b1%22%3E%0D%0A%3Ccircle%20transform%3D%22matrix%282.75%200%200%202.75%20-22.3%20-12.41%29%22%20cy%3D%227.693%22%20cx%3D%2216.71%22%20r%3D%22.6187%22%2F%3E%0D%0A%3Ccircle%20transform%3D%22matrix%282.75%200%200%202.75%20-22.3%2014.81%29%22%20cy%3D%227.693%22%20cx%3D%2216.71%22%20r%3D%22.6187%22%2F%3E%0D%0A%3Ccircle%20transform%3D%22matrix%282.75%200%200%202.75%20-35.91%201.2%29%22%20cy%3D%227.693%22%20cx%3D%2216.71%22%20r%3D%22.6187%22%2F%3E%0D%0A%3Ccircle%20transform%3D%22matrix%282.75%200%200%202.75%20-8.691%201.2%29%22%20cy%3D%227.693%22%20cx%3D%2216.71%22%20r%3D%22.6187%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3Ccircle%20stroke-linejoin%3D%22round%22%20stroke-width%3D%22.7365%22%20stroke-linecap%3D%22round%22%20transform%3D%22matrix%281.358%200%200%201.358%201.77%20-.4937%29%22%20stroke%3D%22%23a5a5a5%22%20cy%3D%2216.91%22%20cx%3D%2216.25%22%20r%3D%2214.91%22%20fill%3D%22none%22%2F%3E%0D%0A%3C%2Fg%3E%0D%0A%3C%2Fg%3E%0D%0A%3C%2Fsvg%3E";
-
-function $e(tag, attrs, text) {
-    let rv = document.createElement(tag);
-    attrs = attrs || {};
-    for (let a in attrs) {
-        rv.setAttribute(a, attrs[a]);
-    }
-    if (text) {
-        rv.textContent = text;
-    }
-    return rv;
-}
-
-const $ = (sel, el) => (el || document).querySelector(sel);
-const $$ = (sel, el) => Array.from((el || document).querySelectorAll(sel));
-const noop = function() {};
-
-let active = false, button, file_list, thumb_list;
-
-(function() {
-    document.body.appendChild($e("style", null, `
-.icon-vnthumb {
-  margin: 0 !important;
-}
-.icon-vnthumb:before {
-  content: "\\f03e"; /* XXX use actual icon class, but colors :*( */
-}
-.volanail-button {
-  position: relative;
-  z-index: 150;
-  font-size: 18px;
-}
-.volanail-button[active] {
-  box-shadow: inset 0px 0px 8px 3px rgba(120, 120, 120, 0.9);
-}
-#volanail-list {
-  display: flex;
-  flex-wrap: wrap;
-  padding: 1em 0.3em;
-}
-.volanail-thumb > img, .volanail-thumb > video {
-  max-height: calc(100% - 1.4em - 4ex);
-  max-width: calc(100% - 1.2em);
-}
-.volanail-thumb > div.volanail-media {
-  height: calc(100% - 1.4em - 8ex);
-  width: calc(100% - 1.2em - 4ex);
-  border: 6px dashed white !important;
-  border-radius: 26px;
-}
-.volanail-thumb > .file_name.file_hellbanned {
-  text-decoration: line-through double;
-  color: #ed7174;
-}
-.volanail-thumb > div.volanail-media > span {
-  margin: auto;
-  padding: 0;
-  font-size: 90px;
-}
-.volanail-thumb {
-  display: inline-flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: calc(100% / 4 - 2.4ex);
-  height: 250px;
-  border-radius: 10px;
-  background: rgba(255,255,255,0.1);
-  padding: 0.5em 0.2em;
-  margin: 0.5ex;
-  text-decoration: none;
-  text-align: center;
-  border: 2px solid rgba(128,128,128,0.3) !important;
-}
-.volanail-thumb .tag_key_ip {
-  display: inline-block;
-  margin-left: 2ex;
-}
-.volanail-checked {
-  border: 2px solid white !important;
-}
-.volanail-video {
-  background: rgba(255,255,255,0.2);
-}
-.volanail-name, .volanail-infos {
-  font-size: small;
-  text-overflow: ellipsis;
-  width: 100%;
-  white-space: nowrap;
-  overflow: hidden;
-  margin-top: 0.4ex;
-}
-.volanail-name .file_control {
-  padding: 0;
-  padding-right: 1ex;
-}
-.volanail-infos {
-  font-size: x-small;
-}
-`));
-
-    let cont = $("#upload_container");
-    button = $e("label", {
-        "for": "volanail-button",
-        "id": "volanail-button",
-        "class": "button volanail-button"
-    });
-    button.appendChild($e("span", {
-        "class": "icon-vnthumb"
-    }));
-    button.appendChild($e("span", {
-        "class": "on_small_header"
-    }/*, Thumbnail"*/));
-    cont.insertBefore(button, cont.firstChild);
-    file_list = $("#file_list");
-    thumb_list = $e("div", {id: "volanail-list"});
-    thumb_list.style.display = "none";
-    file_list.parentElement.insertBefore(thumb_list, file_list);
-})();
-
-const force_update = () => {
-    dry.exts.filelist.update_info.oldUnseenFiles = -1; // force the update m8
-    dry.exts.filelist.scheduleDomUpdate();
-};
 
 const apool = new class AnimationPool {
     constructor() {
@@ -173,6 +49,148 @@ const apool = new class AnimationPool {
         }
     }
 }();
+
+
+function $e(tag, attrs, text) {
+    let rv = document.createElement(tag);
+    attrs = attrs || {};
+    for (let a in attrs) {
+        rv.setAttribute(a, attrs[a]);
+    }
+    if (text) {
+        rv.textContent = text;
+    }
+    return rv;
+}
+
+const $ = (sel, el) => (el || document).querySelector(sel);
+const $$ = (sel, el) => Array.from((el || document).querySelectorAll(sel));
+const noop = function() {};
+
+let active = false, button, file_list, thumb_list;
+
+(function() {
+    let sheet = $e("style", {id: "volanail-sheet"}, `
+.icon-vnthumb {
+  margin: 0 !important;
+}
+.icon-vnthumb:before {
+  content: "\\f03e"; /* XXX use actual icon class, but colors :*( */
+}
+.volanail-button {
+  position: relative;
+  z-index: 150;
+  font-size: 18px;
+}
+.volanail-button[active] {
+  box-shadow: inset 0px 0px 8px 3px rgba(120, 120, 120, 0.9);
+}
+#volanail-list {
+  display: flex;
+  flex-wrap: wrap;
+  padding: 6px 10px;
+}
+.volanail-thumb > img, .volanail-thumb > video {
+  max-height: calc(100% - 1.4em - 4ex);
+  max-width: calc(100% - 1.2em);
+}
+.volanail-thumb > div.volanail-media {
+  height: calc(100% - 1.4em - 8ex);
+  width: calc(100% - 1.2em - 4ex);
+  border: 6px dashed white !important;
+  border-radius: 26px;
+}
+.volanail-thumb > .file_name.file_hellbanned {
+  text-decoration: line-through double;
+  color: #ed7174;
+}
+.volanail-thumb > div.volanail-media > span {
+  margin: auto;
+  padding: 0;
+  font-size: 90px;
+}
+.volanail-thumb {
+  display: inline-flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: calc(100% / 4 - 12px - 1ex);
+  height: 260px;
+  border-radius: 10px;
+  background: rgba(255,255,255,0.1);
+  padding: 0.5em 4px;
+  margin: 0.5ex;
+  text-decoration: none;
+  text-align: center;
+  border: 2px solid rgba(128,128,128,0.3) !important;
+}
+.volanail-thumb .tag_key_ip {
+  display: inline-block;
+  margin-left: 2ex;
+}
+.volanail-checked {
+  border: 2px solid white !important;
+}
+.volanail-video {
+  background: rgba(255,255,255,0.2);
+}
+.volanail-name, .volanail-infos {
+  font-size: small;
+  text-overflow: ellipsis;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  margin-top: 0.4ex;
+}
+.volanail-name .file_control {
+  padding: 0;
+  padding-right: 1ex;
+}
+.volanail-infos {
+  font-size: x-small;
+}
+`);
+    document.body.appendChild(sheet);
+    let cont = $("#upload_container");
+    button = $e("label", {
+        "for": "volanail-button",
+        "id": "volanail-button",
+        "class": "button volanail-button"
+    });
+    button.appendChild($e("span", {
+        "class": "icon-vnthumb"
+    }));
+    button.appendChild($e("span", {
+        "class": "on_small_header"
+    }/*, Thumbnail"*/));
+    cont.insertBefore(button, cont.firstChild);
+    file_list = $("#file_list");
+    thumb_list = $e("div", {id: "volanail-list"});
+    thumb_list.style.display = "none";
+    file_list.parentElement.insertBefore(thumb_list, file_list);
+
+    const files_frame = $("#files_frame");
+    let oldCount = 0;
+    const rule = Array.from(sheet.sheet.cssRules).find(e => e && e.selectorText === ".volanail-thumb" ? e : null);
+    const update_columns = () => {
+        apool.schedule(null, () => {
+            const columnCount = Math.max(2, Math.floor(files_frame.clientWidth / 220));
+            if (oldCount === columnCount) {
+                return;
+            }
+            oldCount = columnCount;
+            rule.style.width = `calc(100% / ${columnCount} - 12px - 1ex)`;
+        });
+    };
+    // Easier to observe mutations than hook into lain codes
+    new MutationObserver(muts => update_columns()).observe(files_frame, {attributes: true});
+    update_columns();
+})();
+
+const force_update = () => {
+    dry.exts.filelist.update_info.oldUnseenFiles = -1; // force the update m8
+    dry.exts.filelist.scheduleDomUpdate();
+};
 
 class Thumbnail {
     constructor(file) {
