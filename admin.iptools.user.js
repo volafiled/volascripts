1138b019af3926d1127d67dd63fb40beba07076e
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2017-09-27 04:23:24 +0200

    Put non-tab tabs where they belong

--- admin.iptools.user.js
+++ admin.iptools.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Vola IP Tools
-// @version      26
+// @version      27
 // @description  Hides ip addresses for mods.
 // @namespace    https://volafile.org
 // @icon         https://volafile.org/favicon.ico
@@ -10,12 +10,16 @@
 // @run-at       document-start
 // ==/UserScript==
 
-dry.once("dom", () => {
-    "use strict";
-    console.log("running", GM_info.script.name, GM_info.script.version, dry.version);
+/* global dry, GM_info */
 
-    const style = document.createElement("style");
-    style.textContent = `
+dry.once("dom", () => {
+  "use strict";
+
+  console.log(
+    "running", GM_info.script.name, GM_info.script.version, dry.version);
+
+  const style = document.createElement("style");
+  style.textContent = `
 body[noipspls] a.username > span:not([class]),
 body[noipspls] a.username > span[class=""],
 body[noipspls] .tag_key_ip {
@@ -48,203 +52,224 @@ body[noipspls] .tag_key_ip {
   right: 0;
   bottom: 0;
 }
+
+#name_container {
+  height: auto;
+  line-height: normal;
+}
+#room_name {
+  font-size: 1.8em;
+}
+#chat_hbar_buttons {
+  display: block;
+  text-align: right;
+  font-size: 80% !important;
+  margin-top: -1ex;
+  padding-right: 1ex !important;
+}
+
 `;
-    document.body.appendChild(style);
-    let state = localStorage.getItem("noipspls") !== "false";
-    if (state !== false) {
-        state = true;
+  document.body.appendChild(style);
+  let state = localStorage.getItem("noipspls") !== "false";
+  if (state !== false) {
+    state = true;
+  }
+  const update = () => {
+    if (state) {
+      document.body.setAttribute("noipspls", "true");
     }
-    const update = () => {
-        if (state) {
-            document.body.setAttribute("noipspls", "true");
-        }
-        else {
-            document.body.removeAttribute("noipspls");
-        }
-        localStorage.setItem("noipspls", state.toString());
-        if (dry.exts) {
-            dry.exts.chat.scrollState.bottom();
-        }
-    };
-    const toggle = () => {
-        state = !state;
-        update();
-    };
+    else {
+      document.body.removeAttribute("noipspls");
+    }
+    localStorage.setItem("noipspls", state.toString());
+    if (dry.exts) {
+      dry.exts.chat.scrollState.bottom();
+    }
+  };
+  const toggle = () => {
+    state = !state;
     update();
+  };
+  update();
 
-    let btn = document.createElement("a");
-    btn.textContent = "IP";
-    btn.style.padding = "0 1ex";
-    let uc = document.querySelector("#user_count_icon");
-    uc.parentElement.insertBefore(btn, uc.nextSibling);
-    btn.addEventListener("click", toggle);
+  const btn = document.createElement("a");
+  btn.textContent = "IP";
+  btn.style.padding = "0 1ex";
+  const uc = document.querySelector("#user_count_icon");
+  uc.parentElement.insertBefore(btn, uc.nextSibling);
+  btn.addEventListener("click", toggle);
 
-    dry.replaceEarly("chat", "showMessage", function(orig, nick, message, options, data, ...args) {
-        try {
-            if (nick === "Log" && options.staff) {
-                if (typeof(message) == "string") {
-                    message = {type: "text", value: message};
+  dry.replaceEarly("chat", "showMessage",
+    function(orig, nick, message, options, data, ...args) {
+      try {
+        if (nick === "Log" && options.staff) {
+          if (typeof (message) === "string") {
+            message = {type: "text", value: message};
+          }
+          if (!message.forEach) {
+            message = [message];
+          }
+          const newmsg = new dry.unsafeWindow.Array();
+          message.forEach(m => {
+            try {
+              if (m && m.type === "text") {
+                let pieces = m.value.match(/^(.+?)( to ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(.+)$/);
+                if (!pieces) {
+                  pieces = m.value.match(/^(.+?)( \(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\))(.+)$/);
                 }
-                if (!message.forEach) {
-                    message = [message];
+                if (!pieces) {
+                  pieces = m.value.match(/^(.+?)( ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]+))(.*?)$/);
                 }
-                let newmsg = new dry.unsafeWindow.Array();
-                message.forEach(m => {
-                    try {
-                        if (m && m.type === "text") {
-                            let pieces = m.value.match(/^(.+?)( to ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(.+)$/);
-                            if (!pieces) {
-                                pieces = m.value.match(/^(.+?)( \(([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})\))(.+)$/);
-                            }
-                            if (!pieces) {
-                                pieces = m.value.match(/^(.+?)( ([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]+))(.*?)$/);
-                            }
-                            if (!pieces) {
-                                newmsg.push(m);
-                                return;
-                            }
-                            newmsg.push({type: "text", value: pieces[1] + pieces[4]});
-                            if (!data) {
-                                data = new dry.unsafeWindow.Object();
-                                data.ip = pieces[3];
-                            }
-                            return;
-                        }
-                    }
-                    catch (ex) {
-                        console.error(ex);
-                    }
-                    newmsg.push(m);
-                });
-                message = newmsg;
+                if (!pieces) {
+                  newmsg.push(m);
+                  return;
+                }
+                newmsg.push({type: "text", value: pieces[1] + pieces[4]});
+                if (!data) {
+                  data = new dry.unsafeWindow.Object();
+                  [,,, data.ip] = pieces;
+                }
+                return;
+              }
             }
-        }
-        catch (ex) {
-            console.error(ex);
-        }
-        const msg = orig(...[nick, message, options, data].concat(args));
-        try {
-            if (msg && msg.ip_elem) {
-                msg.ban_elem = document.createElement("span");
-                const hammer = document.createElement("i");
-                hammer.setAttribute("class", "chat_message_icon icon-hammer");
-                msg.ban_elem.appendChild(hammer);
-                msg.ban_elem.setAttribute("class", "username clickable ban");
-                msg.ban_elem.addEventListener("click", msg.showBanWindow.bind(msg));
-                msg.nick_elem.appendChild(msg.ban_elem);
+            catch (ex) {
+              console.error(ex);
             }
+            newmsg.push(m);
+          });
+          message = newmsg;
         }
-        catch (ex) {
-            console.error(ex);
+      }
+      catch (ex) {
+        console.error(ex);
+      }
+      const msg = orig(...[nick, message, options, data].concat(args));
+      try {
+        if (msg && msg.ip_elem) {
+          msg.ban_elem = document.createElement("span");
+          const hammer = document.createElement("i");
+          hammer.setAttribute("class", "chat_message_icon icon-hammer");
+          msg.ban_elem.appendChild(hammer);
+          msg.ban_elem.setAttribute("class", "username clickable ban");
+          msg.ban_elem.addEventListener("click", msg.showBanWindow.bind(msg));
+          msg.nick_elem.appendChild(msg.ban_elem);
         }
-        return msg;
+      }
+      catch (ex) {
+        console.error(ex);
+      }
+      return msg;
     });
 
-    new class extends dry.Commands {
-        ip(e) {
-            toggle();
-            return true;
-        }
-    }();
-
-    {
-        let settings = document.querySelector("#room_settings");
-        let disable = document.createElement("a");
-        disable.id = "disable_room";
-        disable.className = "button light";
-        disable.textContent = "Nuke";
-
-        settings.parentElement.appendChild(disable);
-        disable.addEventListener("click", function() {
-            dry.exts.ui.showQuestion({
-                title: "Disable this room",
-                text: "Are you sure you want to disable this room?",
-                positive: "Disable",
-                negative: "Abort"
-            }, (res) => {
-                if (!res) {
-                    return;
-                }
-                dry.exts.connection.call("editInfo", {
-                    "name": "closed",
-                    "motd": "",
-                    "disabled": true
-                });
-            });
-        });
+  new class extends dry.Commands {
+    ip() {
+      toggle();
+      return true;
     }
+  }();
 
-    dry.replaceEarly("admin", "showBanWindow", function(orig, ips, uploading, chat, blacklist) {
-        function change(form, reasons) {
-            const reasontype = form.reason_elem.value;
-            const reason = reasons[reasontype];
-            if (reason.custom) {
-                form.mute_elem.checked = !!chat;
-                form.ban_elem.checked = !!uploading;
-                form.creason_elem.value = "";
-                form.hours_elem.value = "1";
-                return;
-            }
-            form.mute_elem.checked = !!reason.mute;
-            form.ban_elem.checked = !!reason.upload;
-            form.hellban_elem.checked = !!reason.hellban;
-            form.hours_elem.value = reason.hours + "";
-            form.creason_elem.value = reason.reason || reason.text;
+  {
+    const settings = document.querySelector("#room_settings");
+    const disable = document.createElement("a");
+    disable.id = "disable_room";
+    disable.className = "button light";
+    disable.textContent = "Nuke";
+
+    settings.parentElement.appendChild(disable);
+    disable.addEventListener("click", function() {
+      dry.exts.ui.showQuestion({
+        title: "Disable this room",
+        text: "Are you sure you want to disable this room?",
+        positive: "Disable",
+        negative: "Abort"
+      }, res => {
+        if (!res) {
+          return;
         }
-        if (!this.isAdmin) {
-            return;
+        dry.exts.connection.call("editInfo", {
+          name: "closed",
+          motd: "",
+          disabled: true
+        });
+      });
+    });
+    const header = document.querySelector("#room_name_container");
+    header.appendChild(disable.parentElement);
+  }
+
+  dry.replaceEarly("admin", "showBanWindow",
+    function(orig, ips, uploading, chat, blacklist) {
+      function change(form, reasons) {
+        const reasontype = form.reason_elem.value;
+        const reason = reasons[reasontype];
+        if (reason.custom) {
+          form.mute_elem.checked = !!chat;
+          form.ban_elem.checked = !!uploading;
+          form.creason_elem.value = "";
+          form.hours_elem.value = "1";
+          return;
         }
-        uploading = !!uploading;
-        chat = !!chat;
-        const tmpl = this.import("templates");
-        const conn = this.import("connection");
-        const reasons = Object.assign({}, tmpl.render("bans"));
+        form.mute_elem.checked = !!reason.mute;
+        form.ban_elem.checked = !!reason.upload;
+        form.hellban_elem.checked = !!reason.hellban;
+        form.hours_elem.value = `${reason.hours}`;
+        form.creason_elem.value = reason.reason || reason.text;
+      }
+
+      if (!this.isAdmin) {
+        return;
+      }
+      uploading = !!uploading;
+      chat = !!chat;
+      const tmpl = this.import("templates");
+      const conn = this.import("connection");
+      const reasons = Object.assign({}, tmpl.render("bans"));
+      if (blacklist) {
+        for (const k of Object.keys(reasons)) {
+          const b = reasons[k];
+          if (!b.upload && !b.hellban && !b.custom) {
+            delete reasons[k];
+            continue;
+          }
+          if (b.upload && b.hours <= 24) {
+            b.upload = false;
+          }
+        }
+      }
+      if (chat || uploading) {
+        for (const k of Object.keys(reasons)) {
+          const b = reasons[k];
+          const n = !!b.default_upload;
+          const s = !!b.default_mute;
+          b.default = s === chat && n === uploading;
+        }
+      }
+      const form = tmpl.renderForm("forms.ban", {
+        ips,
+        uploading,
+        chat,
+        reasons,
+        blacklist
+      });
+      change(form, reasons);
+      form.reason_elem.addEventListener("change", () => change(form, reasons));
+      form.on("submit", function() {
+        this.dismiss();
+        const options = {
+          hours: parseFloat(this.hours),
+          reason: this.creason.trim(),
+          purgeFiles: this.purge || false,
+          ban: this.ban,
+          hellban: this.hellban,
+          mute: this.mute
+        };
         if (blacklist) {
-            for (const k of Object.keys(reasons)) {
-                const b = reasons[k];
-                if (!b.upload && !b.hellban && !b.custom) {
-                    delete reasons[k];
-                    continue;
-                }
-                if (b.upload && b.hours <= 24) {
-                    b.upload = false;
-                }
-            }
+          conn.call("blacklistFiles", ips, options);
+          return;
         }
-        if (chat || uploading) {
-            for (const k of Object.keys(reasons)) {
-                const b = reasons[k];
-                var n = !!b.default_upload;
-                var s = !!b.default_mute;
-                b.default = s === chat && n === uploading;
-            }
+        for (let i = this.ip.split(","), n = 0; n < i.length; n++) {
+          conn.call("banUser", i[n].trim(), options);
         }
-        const form = tmpl.renderForm("forms.ban", {
-            ips,
-            uploading,
-            chat,
-            reasons,
-            blacklist
-        });
-        change(form, reasons);
-        form.reason_elem.addEventListener("change", () => change(form, reasons));
-        form.on("submit", function() {
-            this.dismiss();
-            const options = {
-                hours: parseFloat(this.hours),
-                reason: this.creason.trim(),
-                purgeFiles: this.purge || false,
-                ban: this.ban,
-                hellban: this.hellban,
-                mute: this.mute
-            };
-            if (blacklist) {
-                conn.call("blacklistFiles", ips, options);
-                return;
-            }
-            for (let i = this.ip.split(","), n = 0; n < i.length; n++) {
-                conn.call("banUser", i[n].trim(), options);
-            }
-        });
+      });
     });
 });
