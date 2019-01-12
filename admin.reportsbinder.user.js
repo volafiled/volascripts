c93d3c3c754cd0c5e55e35a00f43d1cb07c11c17
szero <singleton@tfwno.gf>
2019-01-12 03:04:25 +0100

    address review issues

--- admin.reportsbinder.user.js
+++ admin.reportsbinder.user.js
@@ -6,7 +6,7 @@
 // @author       Hefty Chungus
 // @match        https://volafile.org/r/*
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @require      https://raw.githubusercontent.com/volafiled/volascripts/master/dry.js
+// @require      https://cdn.jsdelivr.net/gh/volafiled/volascripts/dry.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -14,31 +14,23 @@
 dry.once("dom", () => {
   'use strict';
   console.log("running", GM.info.script.name, GM.info.script.version, dry.version);
-  const ke = new KeyboardEvent("keydown", {
-    key: "Enter", keyCode: "13"
-  });
-  const bindtab = "bindmehere";
-  const ci = document.getElementById("chat_input");
+  const bindtab = GM.info.script.name;
+  const tabbind = localStorage.getItem(bindtab);
   new class extends dry.Commands {
     bindreports() {
-      localStorage.setItem(bindtab, window.location.href);
+      localStorage.setItem(bindtab, config.room_id);
       dry.appendMessage("Binder", "Reports are bound to this tab now");
-      ci.value = "/reports";
-      ci.dispatchEvent(ke);
+      dry.exts.connection.call("command", dry.exts.user.info.nick, "reports", "")
     }
   }();
   new class extends dry.MessageFilter {
     showMessage(orig, nick, message, options) {
-      let tabbind = localStorage.getItem(bindtab);
-      if ("Log" === nick && tabbind === window.location.href) {
-        if (message[0].hasOwnProperty("value")) {
-          let match = message[0].value.match(`${dry.exts.user.info.nick} joined`);
-          if (!!match && !!dry.exts.user.info.staff) {
-            ci.value = "/reports";
-            ci.dispatchEvent(ke);
-          }
+      if (("Log" === nick || "Network" === nick) && tabbind === config.room_id) {
+        if (!!dry.exts.user.info.admin) {
+          dry.exts.connection.call("command", dry.exts.user.info.nick, "reports", "")
         }
       }
+      return;
     }
   }();
 });
