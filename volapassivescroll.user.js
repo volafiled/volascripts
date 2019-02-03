45d93605ea153ac110e94a23f2e4da44b77d3e7b
szero <singleton@tfwno.gf>
2019-02-03 22:12:46 +0100

    make script actually work

--- volapassivescroll.user.js
+++ volapassivescroll.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name        VolaPassiveScroll
 // @namespace   https://volafile.org
+// @description your passive weapon of choice
 // @include     https://volafile.org/r/*
 // @icon        https://volafile.org/favicon.ico
 // @author      Buddy Boi
@@ -11,14 +12,15 @@
 // @run-at      document-start
 // @version     1
 // ==/UserScript==
-/* globals dry, SimplerScrollbar */
+/* globals dry, SimplerScrollbar, GM */
 dry.once("load", () => {
   "use strict";
-  const $ = document.querySelector;
+  console.log("running", GM.info.script.name, GM.info.script.version);
+  const $ = el => document.querySelector(el);
   const container = $("#files_frame");
   const wrapper = $("#files_scroller");
   const content = $("#files_content");
-  SimplerScrollbar.initEl({container, wrapper, content, rightOffset: 2});
+  SimplerScrollbar.initContainer({container, wrapper, content, rightOffset: 2});
   const new_scrollbar = $(".ss-scroll");
   new_scrollbar.classList.add("scroller-slider");
   wrapper.removeChild($("#files_scroller > .scroller-pane"));
