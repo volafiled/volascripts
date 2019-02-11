699f6db33d1754143e625b434d9a9981daf463db
szero <singleton@tfwno.gf>
2019-02-11 03:18:46 +0100

    simplify stuff according to dolos' accordance

--- titlehover.user.js
+++ titlehover.user.js
@@ -2,11 +2,11 @@
 // @name         StuffOnNameHover
 // @namespace    https://not.jew.dance
 // @version      3
-// @description  Show da owner
+// @description  Show da owner und jannies
 // @author       Your mom
 // @icon         https://volafile.org/favicon.ico
 // @match        https://volafile.org/r/*
-// @require      https://cdn.jsdelivr.net/gh/volafiled/volascripts/dry.js
+// @require      https://cdn.jsdelivr.net/gh/volafiled/volascripts@a9c0424e5498deea9fd437c15b2137c3bec07c61/dry.min.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
@@ -14,28 +14,19 @@
 
 dry.once("load", () => {
   'use strict';
-  String.prototype.capitalize = function() {
-    return this[0].toUpperCase() + this.slice(1).toLowerCase();
-  };
   const room_title = document.getElementById("name_container");
-  let room_owner;
-  let jannies = new Array();
-  dry.exts.connection.once("config", cfg => {
-    if (!cfg.owner) {
-      room_title.title = room_owner = "Room has no owner";
-    }
-  });
-  dry.exts.config.on("config_owner", owner => {
-    if (owner) {
-      room_owner = `${owner.capitalize()} is the room owner`;
-      room_title.title = jannies.length ? `${room_owner}\nJannies: ${jannies}` : room_owner;
+  let room_owner = "";
+  let jannies = "";
+  dry.exts.connection.on("config", cfg => {
+    if (typeof cfg.owner === "string" || room_owner) {
+      room_owner = cfg.owner || room_owner;
     }
     else {
-      room_title.title = room_owner = "Room has no owner";
+      room_owner = "Room has no owner";
     }
-  });
-  dry.exts.config.on("config_janitors", janitors => {
-    jannies = janitors;
-    room_title.title = `${room_owner}${jannies.length ? "\nJannies: " + jannies : ""}`;
+    if (Array.isArray(cfg.janitors)) {
+      jannies = cfg.janitors.length ? `Janitors: ${cfg.janitors}` : "";
+    }
+    room_title.title = `${room_owner} is the room owner\n${jannies}`;
   });
 });
