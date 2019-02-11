233d9646de4d00bd72d5d840cbbe403b6b73d3e3
szero <singleton@tfwno.gf>
2019-02-11 12:39:46 +0100

    change script name

--- titlehover.user.js
+++ titlehover.user.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         StuffOnNameHover
+// @name         Maintainers On Room Title Hover
 // @namespace    https://not.jew.dance
 // @version      3
 // @description  Show da owner und jannies
