b06ac549f736a7ea9a7cef748c757aa72b7d9cdf
Dolores Greatamsky <RealDolos@users.noreply.github.com>
2019-04-02 10:59:36 +0200

    henlo

--- admin.unnotify-reports.user.js
+++ admin.unnotify-reports.user.js
@@ -1,41 +1,50 @@
 // ==UserScript==
 // @name         Unnotify reports
-// @version      5
+// @version      6
 // @description  because it's annoying
 // @namespace    https://volafile.org
 // @icon         https://volafile.org/favicon.ico
 // @author       topkuk productions
 // @match        https://volafile.org/r/*
 // @require      https://greasemonkey.github.io/gm4-polyfill/gm4-polyfill.js
-// @require      https://cdn.rawgit.com/RealDolos/volascripts/1dd689f72763c0e59f567fdf93865837e35964d6/dry.js
+// @require      https://cdn.jsdelivr.net/gh/RealDolos/volascripts@1dd689f72763c0e59f567fdf93865837e35964d6/dry.js
 // @grant        none
 // @run-at       document-start
 // ==/UserScript==
+/* globals dry */
 
 dry.once("dom", () => {
-    "use strict";
-    console.log("running", GM.info.script.name, GM.info.script.version, dry.version);
-    const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip)[ "]|periscopefollower/;
+  "use strict";
+  console.log("running", GM.info.script.name, GM.info.script.version, dry.version);
+  const BLACK = /\.(txt|html?|rtf|docx?|xlsx?|exe|rar|zip)[ "]|periscopefollower/;
 
-    new class extends dry.MessageFilter {
-        showMessage(orig, nick, message, options) {
-            if (typeof message !== "string" && ((nick === "Report" && options.staff) || (message && message[0] && message[0].href === "/reports"))) {
-                options.notify = options.highlight = false;
-                try {
-                    let text = message.map(e => e.value || "").join("");
-                    if (text.includes("BLACKLIST") && BLACK.test(text)) {
-                        console.error(text);
-                        return false;
-                    }
-                    return;
-                }
-                catch (ex) {
-                    console.error("failed", message, ex);
-                }
-            }
-            if (nick === "Log" && options.staff) {
-                options.notify = options.highlight = false;
-            }
+  new class extends dry.MessageFilter {
+    showMessage(orig, nick, message, options) {
+      if (typeof message !== "string" && ((nick === "Report" && options.staff) || (message && message[0] && message[0].href === "/reports"))) {
+        options.notify = options.highlight = false;
+        try {
+          let text = message.map(e => e.value || "").join("");
+          let urls = message.map(e => e.href || "").join(" ");
+          if (text.includes("BLACKLIST") && BLACK.test(text)) {
+            console.error(text);
+            return false;
+          }
+          if (text.includes("παρεακι")) {
+            console.error(text);
+            return false;
+          }
+          if (urls.includes("BEEPi")) {
+            return false;
+          }
+          return;
         }
-    }();
+        catch (ex) {
+          console.error("failed", message, ex);
+        }
+      }
+      if (nick === "Log" && options.staff) {
+        options.notify = options.highlight = false;
+      }
+    }
+  }();
 });
